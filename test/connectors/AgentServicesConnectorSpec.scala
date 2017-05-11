28a390a6f45dcd322764c1156e11d27b322907df
robert-hawkes <robert.hawkes@digital.hmrc.gov.uk>
2017-05-11 13:11:32 +0100

    SAR-628 - Create agent services connector

---
+++ test/connectors/AgentServicesConnectorSpec.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import connectors.mocks.MockAgentServicesConnector
+import play.api.libs.json.Json
+import play.api.test.Helpers._
+import uk.gov.hmrc.play.http.InternalServerException
+
+class AgentServicesConnectorSpec extends MockAgentServicesConnector {
+  "isPreExistingRelationship" should {
+    "return true if the agent and client have a pre existing relationship" in {
+      mockIsPreExistingRelationship(testNino)(OK, Json.toJson(true))
+
+      val res = TestAgentServicesConnector.isPreExistingRelationship(testNino)
+
+      await(res) mustBe true
+    }
+
+    "return false if the agent and client do not have a pre existing relationship" in {
+      mockIsPreExistingRelationship(testNino)(OK, Json.toJson(false))
+
+      val res = TestAgentServicesConnector.isPreExistingRelationship(testNino)
+
+      await(res) mustBe false
+    }
+
+    "return a failure on a non OK status" in {
+      val invalidBody = Json.toJson("invalid")
+      mockIsPreExistingRelationship(testNino)(INTERNAL_SERVER_ERROR, invalidBody)
+
+      val res = TestAgentServicesConnector.isPreExistingRelationship(testNino)
+
+      val ex = intercept[InternalServerException](await(res))
+      ex.getMessage mustBe TestAgentServicesConnector.parsingFailure(INTERNAL_SERVER_ERROR, invalidBody.toString).getMessage
+    }
+  }
+}
---
+++ test/connectors/mocks/MockAgentServicesConnector.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors.mocks
+
+import audit.Logging
+import connectors.AgentServicesConnector
+import play.api.libs.json.JsValue
+
+trait MockAgentServicesConnector extends MockHttp {
+  object TestAgentServicesConnector extends AgentServicesConnector(appConfig, mockHttpGet, app.injector.instanceOf[Logging])
+
+  def mockIsPreExistingRelationship(nino: String)(status: Int, response: JsValue): Unit =
+    setupMockHttpGet(Some(TestAgentServicesConnector.agentClientURL(nino)))(status, Some(response))
+
+  val testNino = "AA123456A"
+}
